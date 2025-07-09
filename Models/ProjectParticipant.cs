ed4bb776f7304fd3edd013be7ec36bae9da663d9
sanUAman <dzivalovoleksandr@gmail.com>
2025-07-09 19:58:48 +0300

    Added some option in gitignore and changed statuses in ProjectParticipant model.

--- Models/ProjectParticipant.cs
+++ Models/ProjectParticipant.cs
@@ -5,20 +5,13 @@ namespace ProjectManage.Models
     public class ProjectParticipant
     {
         public int Id { get; set; }
-
         public int ParticipantId { get; set; }
         public string ParticipantName { get; set; }
         public string ParticipantPassword { get; set; }
-
         public int ProjectId { get; set; }
         public string ProjectName { get; set; }
-
-        [Required]
         public string Role { get; set; }
-
-        [Required]
         public string Tasks { get; set; }
-
         public bool Status { get; set; }
     }
 }
